6819ebb920f84c30f7f6e74e92fa425e37fedd14
John <jotadev@me.com>
2016-03-23 09:52:34 -0300

    add and remove admin

--- public/js/admin.js
+++ public/js/admin.js
@@ -9,3 +9,50 @@ $('.logout').click(function(e) {
         }
     })
 });
+
+$('.save').click(function() {
+    var handle = $('.handle').val();
+    if (handle !== "") {
+        var found = false;
+        $('.handles').each(function() {
+            console.log($(this).find('.username').text(), "@" + handle);
+            if ($(this).find('.username').text() === "@" + handle) {
+                found = true;
+            }
+        });
+
+        if (found) {
+            window.alert('Este usuário já é um administrador');
+        } else {
+            $.post({
+                url: '/admin/users',
+                data: {
+                    handle: handle
+                }
+            }).then(function(handle) {
+                if (handle.success) {
+                    $('.card.admin').eq(1).append(
+                        '<div class="handles"><img src="/img/remove.png" class="remove" data-handle="'+handle.handle+'" /><span class="username">@' + handle.handle + '</span><hr /></div>'
+                    );
+                }
+            });
+        }
+
+    }
+});
+
+$('body').on('click', '.remove', function() {
+    var _this = $(this);
+    if (window.confirm('Tem certeza?')) {
+        $.post({
+            url: '/admin/users/delete',
+            data: {
+                handle: _this.data('handle')
+            }
+        }).then(function(handle) {
+            if (handle.success) {
+                _this.parent().remove();
+            }
+        });
+    }
+});
