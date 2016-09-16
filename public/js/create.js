60c0490e2d16fc9832444167925731e5aba7fb07
Victor Gama <hey@vito.io>
2016-09-16 15:20:23 -0300

    Update create.js

--- public/js/create.js
+++ public/js/create.js
@@ -1,37 +1,65 @@
-$('.addField a').click(function(e) {
-    e.preventDefault();
-    e.stopPropagation();
+// Field adding logic
+$(function() {
+    // Caches
+    var nameSource = $('.newField input'),
+        typeSource = $('.newField select'),
+        newItemSource = $('.toClone'),
+        customFields = $('.customFields');
 
-    var name = $('.newField input').val();
-    var type = $('.newField select option:selected').val();
-    var html = $('.toClone').clone();
-
-    if(name !== '' && type !== 'Type') {
-        customFieldRules(html.find('input'), type);
-
-        html.removeClass('toClone');
-        html.find('input').attr('placeholder', name);
-
-        $('.customFields').append(html);
-
-        $('.newField input').val('');
-        $('.newField select').val('Type');
+    var applyCustomFieldRules = function(input, type) {
+        switch (type) {
+            case 'cpf':
+                input.mask('999.999.999-99');
+                break;
+            default:
+                input.attr('type', type);
+                break;
+        }
     }
-})
 
-$('body').on('click', '.close', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $(this).parent().remove();
-})
+    $('.addField a').click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var name = nameSource.val(),
+            type = typeSource.find('option:selected').val() || 'Type';
 
-function customFieldRules(input, type) {
-    switch (type) {
-        case 'cpf':
-            input.mask('999.999.999-99');
-            break;
-        default:
-            input.attr('type', type);
-            break;
-    }
-}
+        if(name === '' || type === 'Type') {
+            return;
+        }
+
+        var newItem = newItemSource.clone().removeClass('toClone'),
+            newItemField = newItem.find('input').attr('placeholder', name);
+        applyCustomFieldRules(newItemField, type);
+        customFields.append(newItem);
+        typeSource.val('Type');
+        nameSource.val('');
+    });
+});
+
+
+$(function() {
+    $('body').on('click', '.close', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $(this).parent().remove();
+    });
+});
+
+$(function() {
+    $('#createForm').click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var result = {
+            name: $('#campaignName').val(),
+            description: $('#campaignDescription').val(),
+            momentsUrl: $('#momentsLink').val(),
+            confirmationMessage: $('#successMessage').val(),
+            conversionUrl: $('#pixelLink').val(),
+            fields: $('.customFields').find('.field:not(.toClone)').map(function() {
+                    var that = $(this).find('input');
+                    return { name: that.attr('placeholder'), type: that.attr('type') };
+                }).toArray()
+        };
+        console.log(result);
+    });
+});
